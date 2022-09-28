1e3f7e04b261392dfe162afc716fb03869dc4a5c
Jorge Fuster <jorgefuster05@gmail.com>
2022-09-27 21:28:13 -0400

    hdf

--- Assets/script.js
+++ Assets/script.js
@@ -5,71 +5,73 @@ var checklistEl = document.querySelector(".checklist");
 var checklistArray = [];
 
 function init() {
-    // Get stored checklist from local storage
-    var storedChecklist = JSON.parse(localStorage.getItem("checklist"));
+  // Get stored checklist from local storage
+  var storedChecklist = JSON.parse(localStorage.getItem("checklist"));
 
-    // Updates checklist array if there was a stored checklist from local storage
-    if (storedChecklist !== null) {
-        checklistArray = storedChecklist;
-    }
+  // Updates checklist array if there was a stored checklist from local storage
+  if (storedChecklist !== null) {
+    checklistArray = storedChecklist;
+  }
 
-    renderChecklist();
+  renderChecklist();
 }
 
 // Renders checklist items as <li> elements
 function renderChecklist() {
-    checklistEl.innerHTML = "";
+  checklistEl.innerHTML = "";
 
-    for (var i = 0; i < checklistArray.length; i++) {
-        var checklistItemText = checklistArray[i];
+  for (var i = 0; i < checklistArray.length; i++) {
+    var checklistItemText = checklistArray[i];
 
-        var checklistItem = document.createElement("label");
-        checklistItem.classList.add("checkbox");
-        checklistItem.innerHTML = "<input type='checkbox' id='individualCheckbox' data-index='" + i + "'>" + checklistItemText;
-        checklistEl.appendChild(checklistItem);
-    }
+    var checklistItem = document.createElement("label");
+    checklistItem.classList.add("checkbox");
+    checklistItem.innerHTML =
+      "<input type='checkbox' id='individualCheckbox' data-index='" +
+      i +
+      "'>" +
+      checklistItemText;
+    checklistEl.appendChild(checklistItem);
+  }
 }
 
 // Stores checklist array in local storage
 function storeChecklist() {
-    localStorage.setItem("checklist", JSON.stringify(checklistArray));
+  localStorage.setItem("checklist", JSON.stringify(checklistArray));
 }
 
 // When user clicks submit after typing checklist item
-checklistSubmitBtn.addEventListener("click", function(event) {
-    event.preventDefault();
+checklistSubmitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    var item = addChecklistItemEl.value.trim();
-    
-    // Returns from function early if item is blank
-    if(item === "") {
-        return;
-    }
+  var item = addChecklistItemEl.value.trim();
 
-    // Adds checklist item to checklist array and clears input box
-    checklistArray.push(item);
-    addChecklistItemEl.value = "";
+  // Returns from function early if item is blank
+  if (item === "") {
+    return;
+  }
 
-    storeChecklist();
-    renderChecklist();
+  // Adds checklist item to checklist array and clears input box
+  checklistArray.push(item);
+  addChecklistItemEl.value = "";
+
+  storeChecklist();
+  renderChecklist();
 });
 
 // Retrieves any data from local storage and loads (if there are any)
 init();
 
-checklistEl.addEventListener("click", function(event) {
-    var element = event.target;
-  
-    // Checks if element is a checkbox
-    if (element.matches("#individualCheckbox") === true) {
-      // Get its data-index value and remove the checklist element from the list
-      var index = element.getAttribute("data-index");
-      console.log(index);
-      checklistArray.splice(index, 1);
-      console.log(checklistArray);
-  
-      // Store updated checklist array in localStorage, re-render the list
-      storeChecklist();
-      renderChecklist();
-    }
-  });
+checklistEl.addEventListener("click", function (event) {
+  var element = event.target;
+
+  // Checks if element is a checkbox
+  if (element.matches("#individualCheckbox") === true) {
+    // Get its data-index value and remove the checklist element from the list
+    var index = element.getAttribute("data-index");
+    console.log(index);
+
+    // Store updated checklist array in localStorage, re-render the list
+    storeChecklist();
+    renderChecklist();
+  }
+});
